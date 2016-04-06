95cfc21bf118c01f632425e3fd588346fa0d023a
Zachary Murphy <murphyz@my.wccc.edu>
2016-04-05 20:06:06 -0400

    Took it a step further than the book
    The book can be boring sometimes!

--- Word Set/src/word/set/WordSet.java
+++ Word Set/src/word/set/WordSet.java
@@ -19,31 +19,38 @@ import static jdk.nashorn.tools.ShellFunctions.input;
  */
 public class WordSet {
    
-    //use this for user input
-    private static BufferedReader reader;
-    
-    
-    
-    //will be used to put string input into string set array output
-    private static Set<String> arrayOutput;
-    
-    //was going to use this guy
-    //Scanner keyboard = new Scanner(System.in); 
-    
     //main class need ioexception for readLine() method
     public static void main(String args[] ) throws IOException {
+        Scanner keyboard = new Scanner(System.in);
         //basically making reader do same thing as keyboard in
-        reader = new BufferedReader (new InputStreamReader(System.in));
+        BufferedReader reader = new BufferedReader (new InputStreamReader(System.in));
+        
+        //allouwing the user to pick how many lines they want to enter
+        System.out.println("Enter the number of lines you would like to convert to an array.");
+        int amount = keyboard.nextInt();
+        
+        //for loop to control this
+        for(int i = 1; i <= amount; i++ ){
         //some output to tell user what to do
         System.out.println("Enter a line of something..");
         
         //variable set from input
         String userInput =reader.readLine();
         
+        //convert to all lower case even if this is already done by user
+        String input =  userInput.toLowerCase();
+      
+        
         //seperating using split will make array with spaces
-        arrayOutput = new TreeSet<String>(Arrays.asList(userInput.split(" ")));
+        Set <String> arrayOutput = new TreeSet<String>(Arrays.asList(input.split(" ")));
+        
         //wll output in order
-        System.out.println(arrayOutput);
+        System.out.println("Array # " + i + arrayOutput);
+        //makes it look a little nicer
+        System.out.println();
+        }
+        //the end!
+        System.out.println("You have gotten all your arrays.");
     }
     
 }
